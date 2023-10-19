7d9bd1bad3449a3ad0537bd0ad5ddf79bd88da13
akolkarsagar1@gmail.com <akolkarsagar1@gmail.com>
2023-10-19 13:12:46 +0300

    Updated opa files

--- terraform.sh
+++ terraform.sh
@@ -1,5 +1,6 @@
 terraform init
 terraform plan -out planfile.binary
-terraform show -json planfile.binary > opa-input.json
+terraform show -json planfile.binary > plan-output.json
+jq -s '.[0] * .[1]' plan-output.json opa-exclusions.json > opa-input.json
 chmod +x opa
 ./opa eval -i opa-input.json -d policy.repo 'data.terraform'
