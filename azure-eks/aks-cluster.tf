b27f11d78cee3ff9eb4dd30179c130dcc839ba61
SCA2019 <46392733+SCA2019@users.noreply.github.com>
2024-03-03 10:09:41 +0530

    Update aks-cluster.tf

--- azure-eks/aks-cluster.tf
+++ azure-eks/aks-cluster.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_kubernetes_cluster" "aks-monitoring" {
+resource "azurerm_kubernetes_cluster" "default" {
   name                = "aks-monitoring"
   location            = "East US"
   resource_group_name = ""
@@ -34,56 +34,3 @@ resource "azurerm_kubernetes_cluster" "aks-monitoring" {
     environment = "Demo"
   }
 }
-
-resource "azurerm_kubernetes_cluster" "aks-sit" {
-  name                = "aks-sit"
-  location            = "East US"
-  resource_group_name = ""
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
-  kubernetes_version  = "1.26.10"
-
-  default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_B2ms"
-    os_disk_size_gb = 30
-  }
-
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
-
-  role_based_access_control_enabled = true
-
-  tags = {
-    environment = "Demo"
-  }
-}
-
-resource "azurerm_kubernetes_cluster" "aks-uat" {
-  name                = "aks-uat"
-  location            = "East US"
-  resource_group_name = ""
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
-  kubernetes_version  = "1.26.10"
-
-  default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_B2ms"
-    os_disk_size_gb = 30
-  }
-
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
-
-  role_based_access_control_enabled = true
-
-  tags = {
-    environment = "Demo"
-  }
-}
-
